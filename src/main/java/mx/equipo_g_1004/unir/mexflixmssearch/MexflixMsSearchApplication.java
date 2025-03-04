fc57191fff2bf91edfdbdee2821e21ca208d41ef
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-04 00:42:48 -0600

    initialize Mexflix MS Search application with basic structure and configuration

---
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmssearch/MexflixMsSearchApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexflixMsSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MexflixMsSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/equipo_g_1004/unir/mexflixmssearch/MexflixMsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexflixMsSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
